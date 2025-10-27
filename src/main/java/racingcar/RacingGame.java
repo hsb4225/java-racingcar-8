5e18722d657d3fb819b738d6604e509584a25c7c
형성빈 <hyeongseongbin@hyeongseongbin-ui-MacBookAir.local>
2025-10-27 20:34:54 +0900

    feat(RacingGames): 자동차 경주 게임 클래스

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -2,6 +2,7 @@ package racingcar;
 import java.util.*;
 public class RacingGame {
     private List<Car> cars;
+    private static int STRAIGHT_DETECTION_NUMBER = 4;
 
     public RacingGame(String[] carNames) {
         this.cars = new ArrayList<>();
@@ -9,4 +10,18 @@ public class RacingGame {
             cars.add(new Car(carName.trim()));
         }
     }
+
+    public void straightCar(int number) {
+        for(Car car : cars) {
+            RandomNumber randomNumber = new RandomNumber();
+            int ranNumber = randomNumber.getNumber();
+
+            car.straight(ranNumber >= STRAIGHT_DETECTION_NUMBER);
+        }
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+    
 }
